c4076adf04ff2135c8464a2a74df1516ec4dd0b7
achraf saoud <achraf.s@emworks-tn.com>
2024-01-24 14:48:36 +0100

    update

--- src/main/java/com/company/delivery/DTO/CustomerRequestDTO.java
+++ src/main/java/com/company/delivery/DTO/CustomerRequestDTO.java
@@ -3,15 +3,16 @@ package com.company.delivery.DTO;
 import com.company.delivery.entity.Address;
 import com.company.delivery.entity.FoodOrder;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class CustomerRequestDTO {
+    @NotBlank
     private String customerEmail;
 
     private String firstName;
-
 
     private String lastName;
 
